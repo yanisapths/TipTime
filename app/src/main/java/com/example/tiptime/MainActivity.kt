27f05296c162eb26d42f770af6706ab082ebb6ec
yanisapths <72002605+yanisapths@users.noreply.github.com>
2022-07-03 12:51:20 +0700

    calculate the tip done

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.tiptime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    private fun calculateTip() {
+       val stringInTextField = binding.costOfService.text.toString()
+       val cost = stringInTextField.toDoubleOrNull()
+        if (cost == null) {
+            binding.tipAmount.text = ""
+            return
+        }
+        val tipPercentage = when(binding.tipOptions.checkedRadioButtonId) {
+            R.id.amazing_service -> 0.20
+            R.id.good_service -> 0.18
+            else -> 0.15
+        }
+        var tip = tipPercentage * cost
+        val roundUp = binding.roundupTip.isChecked
+        if (roundUp) {
+            tip = kotlin.math.ceil(tip)
+        }
+
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipAmount.text = getString(R.string.tip_amount, formattedTip)
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener{ calculateTip() }
     }
+
 }
